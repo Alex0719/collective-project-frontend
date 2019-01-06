e0e2523b7410aaf7428c1f86d9916dcab0103d08
Deeathex <andreea_ciforac@yahoo.com>
2019-01-06 22:01:36 +0200

    Deeathex: Modificare continut pagina principala. Adaugare directa de trimitere din mail de pagina pentru autentificare companii. Aduagare logo-uri companii in pagina.

--- app/components/pages/Dashboard.js
+++ app/components/pages/Dashboard.js
@@ -9,7 +9,8 @@ import {
 } from 'constants/colors';
 import ChartWithText from '../elements/ChartWithText/ChartWithText';
 
-import { CompanyLogo } from '../../images/companies';
+import { CompanyLogo, AccesaLogo, AlgotechLogo, ArobsLogo, BoschLogo, FortechLogo, SiemensLogo, ToraLogo, 
+  YardiLogo, YonderLogo } from '../../images/companies';
 
 const CirclesContainer = styled.div`
   padding: 40px 0;
@@ -51,11 +52,15 @@ const Question = styled.div`
   div {
     color: ${PEWTER_BLUE};
   }
+  a {
+    text-align: center;
+  }
 
   div,
   h2 {
     display: flex;
     justify-content: center;
+    text-align: center;
   }
 
   min-width: 260px;
@@ -78,8 +83,8 @@ const Partner = styled.div`
 `;
 
 const Img = styled.img`
-  width: 200px;
-  height: 200px;
+  width: auto;
+  height: auto;
 `;
 
 const PartnersH1 = styled.h1`
@@ -115,71 +120,93 @@ export default class DashboardComponent extends React.Component {
         <QuestionContainer>
           <Row>
             <Question>
-              <h2>Ce este Internlink</h2>
-              <div>platforma idk</div>
+              <h2>Cui se adresează?</h2>
+              <div> 
+                Această platformă se adresează companiilor și studenților, indiferent de specializare, din toată țara.
+              </div>
             </Question>
 
             <Question>
-              <h2>Cui se adreseaza?</h2>
-              <div>Oricui</div>
+              <h2>Ce este Internlink?</h2>
+              <div>Internlink este o platformă online pentru gestionarea și promovarea internship-urilor,
+                creată pentru a facilita accesul la informații pentru studenții dornici de a găsi un internship și 
+                pentru companiile care doresc să publice mai ușor detalii despre posturile de internship disponibile.
+              </div>
             </Question>
 
             <Question>
-              <h2>Cui se adreseaza?</h2>
+              <h2>Costă să particip?</h2>
               <div>
-                Raspuns mai lung care s-ar putea sa nu incapa aici dar sa speram
+                Această platformă este gratuită și așa va rămâne de acum încolo.
               </div>
             </Question>
           </Row>
 
           <Row>
             <Question>
-              <h2>Ce este Internlink</h2>
-              <div>platforma idk</div>
+              <h2>Cum mă pot înregistra?</h2>
+              <div>Dacă ești student, accesând secțiunea de înregistrare cont.
+              Conturile implicite create la înregistarea pe site sunt conturi destinate studenților.
+              Dacă ești companie, contactând administratorul de sistem al site-ului sau trimițând un mail la:
+              </div>
+              <a href="mailto:interlink.application@gmail.com?subject=Cont nou companie">interlink.application@gmail.com</a>
             </Question>
 
             <Question>
-              <h2>Cui se adreseaza?</h2>
-              <div>Oricui</div>
+              <h2>Ce este un internship?</h2>
+              <div>Internship-ul este un program pe o durată predefinită, de obicei mai lungă (de la câteva săptămâni până
+                la câteva luni), prin care firmele își recrutează viitori angajați. Pentru studenți reprezintă o 
+                oportunitate de învățare și o modalitate prin care poți obține experiență practică în domeniul de interes.
+              </div>
             </Question>
 
             <Question>
-              <h2>Cui se adreseaza?</h2>
-              <div>Oricui</div>
+              <h2>Cum mă pot înscrie la un internship?</h2>
+              <div>Pentru a putea aplica la un internship este nevoie de crearea unui cont, după care se poate
+                vizualiza o pagină cu anunțuri de internship-uri din partea firmelor și numărul locurilor disponibile,
+                pagină din care se va putea aplica direct pentru internship-ul dorit.
+              </div>
             </Question>
           </Row>
         </QuestionContainer>
         <PartnersH1>Firme partenere</PartnersH1>
         <Partners>
           <Partner>
-            <Img src={CompanyLogo} />
-            companie 1
+            <Img src={AccesaLogo} />
           </Partner>
 
           <Partner>
-            <Img src={CompanyLogo} />
-            companie 1
+            <Img src={AlgotechLogo} />
           </Partner>
 
           <Partner>
-            <Img src={CompanyLogo} />
-            companie 1
+            <Img src={ArobsLogo} />
           </Partner>
 
           <Partner>
-            <Img src={CompanyLogo} />
-            companie 1
+            <Img src={BoschLogo} />
           </Partner>
 
           <Partner>
-            <Img src={CompanyLogo} />
-            companie 1
+            <Img src={FortechLogo} />
           </Partner>
 
           <Partner>
-            <Img src={CompanyLogo} />
-            companie 1
+            <Img src={SiemensLogo} />
           </Partner>
+
+          <Partner>
+            <Img src={ToraLogo} />
+          </Partner>
+
+          <Partner>
+            <Img src={YardiLogo} />
+          </Partner>
+
+          <Partner>
+            <Img src={YonderLogo} />
+          </Partner>
+
         </Partners>
       </div>
     );
