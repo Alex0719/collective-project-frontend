55be4088f96c107f37a51eace45e7c026cba2c26
Laura87510 <laurascurtu1997@yahoo.com>
2019-01-09 18:14:27 +0200

    Resolve bugs

--- app/reducers/studentsReducer.js
+++ app/reducers/studentsReducer.js
@@ -10,14 +10,12 @@ export const initialState = fromJS({
   student: {},
 });
 const studentsReducer = (state = initialState, action) => {
-  console.log('reducer', action.response);
   switch (action.type) {
     case GET_STUDENT_SUCCESS:
       // case UPDATE_STUDENT_SUCCESS:
       return state.merge(fromJS({ student: action.response }));
     case GET_STUDENT_FAILURE:
     case UPDATE_STUDENT_FAILURE:
-      console.log(action.message);
       return state;
     default:
       return state;
